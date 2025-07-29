8204199b7e03921f438f9961291ee9bb998a63db
TouatiAch <twetiachref2@gmail.com>
2025-07-29 18:51:25 +0100

    checking redirect url 4

--- src/auth/msalInstance.ts
+++ src/auth/msalInstance.ts
@@ -2,6 +2,8 @@
 import { PublicClientApplication } from "@azure/msal-browser";
 import { msalConfig } from "../authConfig";
 
+console.log("MSAL will use redirectUri:", msalConfig.auth.redirectUri);
+
 export const msalInstance = new PublicClientApplication(msalConfig);
 // Properly initialize
 await msalInstance.initialize();
